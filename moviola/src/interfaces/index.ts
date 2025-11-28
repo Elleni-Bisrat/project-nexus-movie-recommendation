bbce4f86df0ba5581e66b7fee78b43bdd22294d9
Elleni Bisrat <ellenibisrat16@gmail.com>
2025-11-28 19:55:28 +0300

    Add Movie and WatchlistContextType interfaces

--- moviola/src/interfaces/index.ts
+++ moviola/src/interfaces/index.ts
@@ -4,3 +4,27 @@ export interface MovieCardProps {
   poster: string;
   rating: number;
 }
+export interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  backdrop_path: string;
+  release_date: string;
+  vote_average: number;
+  overview: string;
+  addedAt?: string;
+}
+export interface WatchlistContextType {
+  watchlist: Movie[];
+  addToWatchlist: (movie: Movie) => void;
+  removeFromWatchlist: (movieId: number) => void;
+  isInWatchlist: (movieId: number) => boolean;
+  clearWatchlist: () => void;
+}
+export interface FavoriteList{
+  favoritelist:Movie[];
+  addTOFavoritelist:(movie:Movie)=>void;
+  removeFromFavoritelist:(moviId:number)=>void;
+  isFavorite:(moviId:number)=>boolean;
+  clearFavorite:()=>void;
+}
