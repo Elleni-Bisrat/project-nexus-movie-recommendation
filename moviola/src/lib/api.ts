0c1118428625023a795242027b3cf6386fd0a7e6
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-11-29 11:36:04 +0300

    making the request to reach the server

--- moviola/src/lib/api.ts
+++ moviola/src/lib/api.ts
@@ -1,14 +1,26 @@
 const BASE_URL = "https://api.themoviedb.org/3";
-export async function fetchTrendingMovies(){
-    const res= await fetch(`${BASE_URL}/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`);
-    if(!res.ok) throw new Error("Failed to fetch trending movies");
-    return res.json();
+export async function fetchTrendingMovies() {
+  const res = await fetch(
+    `${BASE_URL}/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+    {
+      headers: { accept: "application/json" }
+    }
+  );
+  if (!res.ok) throw new Error("Failed to fetch trending movies");
+  return res.json();
 }
-export async function fetchMovieDetails(){
-  const res = await fetch(`${BASE_URL}/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`);
-  if(!res.ok) throw new Error("Failed to fetch trending movies");
-    return res.json();
+
+export async function fetchMovieDetails() {
+  const res = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+    {
+      headers: { accept: "application/json" }
+    }
+  );
+  if (!res.ok) throw new Error("Failed to fetch popular movies");
+  return res.json();
 }
+
 
 export const searchMovies = async (query: string) => {
   try {
@@ -16,11 +28,9 @@ export const searchMovies = async (query: string) => {
     const response = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}&page=1&include_adult=false`
     );
-    
     if (!response.ok) {
       throw new Error('Failed to search movies');
     }
-    
     return await response.json();
   } catch (error) {
     console.error('Error searching movies:', error);
